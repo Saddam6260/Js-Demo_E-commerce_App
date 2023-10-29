64ea9d62cb7f2da86278d49230a194d30e3bca9f
Saddam Hafiz Alif <mr.alif.dev@gmail.com>
2023-10-29 23:38:31 +0600

    add card content

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// Latest Offer cards
+let offerCards = [
+  {
+    header: "20% Off On Tank Tops",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+  },
+  {
+    header: "Latest Eyewear For You",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+  },
+  {
+    header: "Let's Lorem Suit Up!",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac dictum.",
+  },
+];
+
 // Selector
 const showBtn = document.querySelector(".showBtn");
 const hideBtn = document.querySelector(".hideBtn");
@@ -8,18 +27,26 @@ showBtn.addEventListener("click", showMenuber);
 hideBtn.addEventListener("click", hideMenuber);
 
 
+// Windows Start
+window.addEventListener("DOMContentLoaded", displayLatestOffer());
 
 /*
 ===========
 Functions
 ===========
-*/ 
+*/
 
 // Show Hide Menuber Function
 function showMenuber() {
-    menuber.classList.add("show-menuber");
+  menuber.classList.add("show-menuber");
 }
 // Hide MenuBer
 function hideMenuber() {
-    menuber.classList.remove("show-menuber");
+  menuber.classList.remove("show-menuber");
+}
+
+function displayLatestOffer() {
+  offerCards.map((card) => {
+    return ``
+  })
 }
